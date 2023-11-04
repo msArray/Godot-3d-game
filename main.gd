6ea7380bfbd70f64939a05a7229b6bac531acdeb
K93MSY <masaya93@ogaki-tv.ne.jp>
2023-11-04 19:28:29 +0900

    FullScreen機能を追加 F11 Key

--- main.gd
+++ main.gd
@@ -10,3 +10,9 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("Open_Menu"):
 		get_tree().quit()
+	
+	if Input.is_action_just_pressed("FullScreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_MAXIMIZED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
