f9b7102e372fc33e031acf6d2a479c7bcf0d1c70
K93MSY <masaya93@ogaki-tv.ne.jp>
2023-11-06 00:04:21 +0900

    試験的なwebsocketの導入

--- main.gd
+++ main.gd
@@ -1,10 +1,12 @@
 extends Node
 
+@export var websocket_url = "ws://127.0.0.1:3000"
+
+var _client = WebSocketPeer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	_client.connect_to_url(websocket_url)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -19,3 +21,18 @@ func _process(delta):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+	
+	_client.poll()
+	
+	var state = _client.get_ready_state()
+	if state == WebSocketPeer.STATE_OPEN:
+		while _client.get_available_packet_count():
+			var data = _client.get_packet().get_string_from_utf8()
+			print("Packet: ", JSON.parse_string(data).id)
+	elif state == WebSocketPeer.STATE_CLOSING:
+		pass
+	elif state == WebSocketPeer.STATE_CLOSED:
+		var code = _client.get_close_code()
+		var reason = _client.get_close_reason()
+		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		set_process(false) # Stop processing.
